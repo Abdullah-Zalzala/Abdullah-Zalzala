a89dd913e7f1b25c3824f122d4a12565542d1f91
Abdullah Zalzala <abdp360@gmail.com>
2025-08-25 15:07:00 +0000

    It Works

---
+++ m1t1.cpp
@@ -0,0 +1,18 @@
+/*
+m1t1 - hello, world!
+Norissa, 8/25/25
+
+Input: Run The Program
+Process: None Yet
+Output: return 0 (no error)
+*/
+
+
+// Magic words to start
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
