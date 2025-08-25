0d76ca1cd850b02cb2e53a341ce9ca50bad337f4
Abdullah Zalzala <abdp360@gmail.com>
2025-08-25 15:26:01 +0000

    Capitals Matter

--- m1t1.cpp
+++ m1t1.cpp
@@ -5,14 +5,19 @@ Norissa, 8/25/25
 Input: Run The Program
 Process: None Yet
 Output: return 0 (no error)
+SPECIAL FX: Greet The User.
 */
-
 
 // Magic words to start
 #include <iostream>
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+    // SFX: Greet The User
+    // Strings Hold Words
+    string First_Name = "Abdullah";
+
+    cout << "Hello, " << First_Name << endl;
+    // output
+    return 0; // No Errors
 }
