6ed5041a5289fca1939abb8b4d6e535321435e1a
Abdullah Zalzala <abdp360@gmail.com>
2025-09-03 15:09:54 +0000

    watermelon shop

---
+++ m1hw1.cpp
@@ -0,0 +1,28 @@
+/* 
+scs 134
+m1hw1
+Abdullah 
+8/27/2025
+Purpose: The "IT" (Replace Noun)
+
+*/
+
+// Magic words
+#include <iostream>
+using namespace std;
+
+int main () {
+    // start program
+    // declare all variables ("nouns")
+// The movie IT 
+// Came out in 2017
+// Made 704.2 million Gross
+
+cout << "The movie IT Came out in 2017 Made 704.2 million Gross" << endl;
+Cout << "My Favorite quote was kimmiaye 
+    // end program
+    return 0;
+
+}
+
+   
