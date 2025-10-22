dee17f0521f3c009d0556522548d967238c8b003
Abdullah Zalzala <abdp360@gmail.com>
2025-10-22 15:00:34 +0000

    m5t1

---
+++ M4/M4T2.cpp
@@ -0,0 +1,26 @@
+/*
+Abdullah Zalzala
+M4T2.cpp
+10/22/2025
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+int main(){
+
+    
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ M5/M5T1.cpp
@@ -0,0 +1,44 @@
+/*
+Abdullah Z
+10/22/2025
+m5t1
+*/
+#include <iostream>
+using namespace std;
+
+
+void say_hello();
+
+int get_the_answer();
+
+double double_a_number(double);
+
+int main() {
+
+double my_num;
+int another_num;
+
+say_hello();
+cout << " please enter a number with or without a decimal point: " << endl;
+cin >> my_num;
+my_num = my_num * 2;
+cout << "double your number is: "<< my_num << endl;
+cout << "but the only answer youll need is: ";
+cout << get_the_answer() << endl;
+
+    return 0;
+}
+void say_hello() {
+
+    cout << "welcome to the best program ever!" << endl;
+}
+
+int get_the_answer() {
+
+
+}
+
+double double_a_number(double the_num){
+    double answer = the_num * 2;
+    return answer;
+}
