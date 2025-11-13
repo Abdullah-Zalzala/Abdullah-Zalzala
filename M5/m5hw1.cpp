4bfc457378653ec01a88964f27ff4d3f0a5b236c
Abdullah Zalzala <abdp360@gmail.com>
2025-11-13 18:36:29 +0000

    m5hw1 q3

--- M5/m5hw1.cpp
+++ M5/m5hw1.cpp
@@ -64,3 +64,37 @@ void question2() {
     cout << "The volume of the block is: " << volume << endl << endl;
 }
 
+
+
+// ---------------------------------------------
+// QUESTION 3: Roman Numeral Converter
+// ---------------------------------------------
+void question3() {
+    int num;
+    cout << "Enter a number (1 - 10): ";
+    cin >> num;
+
+    if (num < 1 || num > 10) {
+        cout << "Error: Number must be 1 through 10." << endl << endl;
+        return;
+    }
+
+    cout << "The Roman numeral version of " << num << " is ";
+
+    switch (num) {
+        case 1:  cout << "I"; break;
+        case 2:  cout << "II"; break;
+        case 3:  cout << "III"; break;
+        case 4:  cout << "IV"; break;
+        case 5:  cout << "V"; break;
+        case 6:  cout << "VI"; break;
+        case 7:  cout << "VII"; break;
+        case 8:  cout << "VIII"; break;
+        case 9:  cout << "IX"; break;
+        case 10: cout << "X"; break;
+    }
+
+    cout << "." << endl << endl;
+}
+
+
